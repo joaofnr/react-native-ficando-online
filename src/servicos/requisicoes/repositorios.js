6ac4dd48eb2d9fab0be76a81117fba1d221c75fe
João Rios <joao.rios@compasso.com.br>
2023-03-09 09:36:24 -0300

    Editando repositório

--- src/servicos/requisicoes/repositorios.js
+++ src/servicos/requisicoes/repositorios.js
@@ -9,3 +9,18 @@ export async function pegarRepositoriosDoUsuario(id){
         return []
     }
 }
+
+export async function salvarRepositorioDoUsuario(postId, name, data, id){
+    try{
+        await api.put(`/repos/${id}`, {
+            postId,
+            name,
+            data,
+            id
+        })
+        return 'sucesso'
+    } catch (error) {
+        console.log(error)
+        return 'erro'
+    }
+}
